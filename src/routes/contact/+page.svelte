e412138a0426164643d4b63c0e888ad7f6261d43
Garik Asplund <garik.asplund@gmail.com>
2024-10-28 11:57:01 -0700

    title change

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,15 +1,17 @@
 <div class="relative h-full w-full bg-black">
-    <enhanced:img src="/static/frontal.png" class="h-full w-full object-cover object-fit" />
-    <section class="absolute inset-0 mx-auto flex flex-col items-center justify-center space-y-8 bg-black/60 px-4 text-justify text-xl md:text-3xl leading-loose text-white/70 md:px-8 lg:px-16">
-        <p>
-            <a href="tel:+17404039921" class="hover:text-orange-500 transition-colors duration-300 md:hidden">
-                740-403-9921
-            </a>
-			<span class="hidden md:block">                740-403-9921
-			</span>
-        </p>
-        <p>
-                jasonporter16@gmail.com
-        </p>
-    </section>
+	<enhanced:img src="/static/frontal.png" class="object-fit h-full w-full object-cover" />
+	<section
+		class="absolute inset-0 mx-auto flex flex-col items-center justify-center space-y-8 bg-black/60 px-4 text-justify text-xl leading-loose text-white/70 md:px-8 md:text-3xl lg:px-16"
+	>
+		<p>
+			<a
+				href="tel:+17404039921"
+				class="transition-colors duration-300 hover:text-orange-500 md:hidden"
+			>
+				740-403-9921
+			</a>
+			<span class="hidden md:block"> 740-403-9921 </span>
+		</p>
+		<p>jasonporter16@gmail.com</p>
+	</section>
 </div>
