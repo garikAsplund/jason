30fbe9a619e4d56bf15ac4567a4861ed1c84b40c
Garik Asplund <garik.asplund@gmail.com>
2024-10-27 23:13:01 -0700

    added youtube

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,25 +1,38 @@
-<nav
-	class="hidden h-full flex-col items-center justify-center font-mono text-4xl text-white text-opacity-85 md:flex md:w-1/4"
->
-	<ul class="space-y-4 p-4">
-		<li class="p-2">
-			<a href="/" class="hover:opacity-75">Home</a>
-		</li>
-		<li class="p-2">
-			<a href="/about" class="hover:opacity-75">About</a>
-		</li>
-		<li class="p-2">
-			<a href="/vinyl" class="hover:opacity-75">Vinyl</a>
-		</li>
-		<li class="p-2">
-			<a href="/watch" class="hover:opacity-75">Watch</a>
-		</li>
-		<li class="p-2">
-			<a href="/podcasts" class="hover:opacity-75">Podcasts</a>
-		</li>
-		<li class="p-2">
-			<a href="/contact" class="hover:opacity-75">Contact</a>
-		</li>
-	</ul>
-</nav>
+<script lang="ts">
+    import { page } from '$app/stores'; 
+</script>
 
+<nav class="hidden h-full flex-col items-center justify-center font-mono text-4xl text-white text-opacity-85 md:flex md:w-1/4">
+    <ul class="space-y-4 p-4">
+        <li class="p-2">
+            <a href="/" class="hover:opacity-75">
+                Home<span class="transition-opacity {$page.url.pathname === '/' ? 'opacity-100' : 'opacity-0'}"> /</span>
+            </a>
+        </li>
+        <li class="p-2">
+            <a href="/about" class="hover:opacity-75">
+                About<span class="transition-opacity {$page.url.pathname === '/about' ? 'opacity-100' : 'opacity-0'}"> /</span>
+            </a>
+        </li>
+        <li class="p-2">
+            <a href="/vinyl" class="hover:opacity-75">
+                Vinyl<span class="transition-opacity {$page.url.pathname === '/vinyl' ? 'opacity-100' : 'opacity-0'}"> /</span>
+            </a>
+        </li>
+        <li class="p-2">
+            <a href="/watch" class="hover:opacity-75">
+                Watch<span class="transition-opacity {$page.url.pathname === '/watch' ? 'opacity-100' : 'opacity-0'}"> /</span>
+            </a>
+        </li>
+        <li class="p-2">
+            <a href="/podcasts" class="hover:opacity-75">
+                Podcasts<span class="transition-opacity {$page.url.pathname === '/podcasts' ? 'opacity-100' : 'opacity-0'}"> /</span>
+            </a>
+        </li>
+        <li class="p-2">
+            <a href="/contact" class="hover:opacity-75">
+                Contact<span class="transition-opacity {$page.url.pathname === '/contact' ? 'opacity-100' : 'opacity-0'}"> /</span>
+            </a>
+        </li>
+    </ul>
+</nav>
