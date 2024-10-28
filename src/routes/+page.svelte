973f6da396b5d6187043f052f8653d2fe74e47e3
Garik Asplund <garik.asplund@gmail.com>
2024-10-27 20:45:43 -0700

    title added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<div class="h-full w-full bg-black">
-	<enhanced:img src="/static/JPP.jpeg" class="object-cover object-fit w-full h-full "/>
+<div class="relative h-full w-full bg-black">
+	<enhanced:img src="/static/JPP.jpeg" class="object-cover w-full h-full" />
+    <div class="absolute inset-0 flex items-start pt-12 md:pt-0 md:items-center justify-center text-white/40 text-center">
+        <h1 class="text-9xl font-bold">Toska Bear</h1>
+    </div>
 </div>
