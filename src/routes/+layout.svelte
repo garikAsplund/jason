8ee971aa6d9f6e75953056911c8b3fb12cd2b7b3
Garik Asplund <garik.asplund@gmail.com>
2024-10-28 20:00:22 -0700

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,10 @@
 	import Icons from '$lib/Icons.svelte';
 	import Nav from '$lib/Nav.svelte';
 	import MobileNav from '$lib/MobileNav.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 
